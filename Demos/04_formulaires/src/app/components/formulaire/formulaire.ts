f7077e6f4e774c8c6d2b511d7b3096f904e9a561
Christophe-Utopios <christophe.r@utopios.solutions>
2025-11-18 11:17:24 +0100

    demo formulaire

--- Demos/04_formulaires/src/app/components/formulaire/formulaire.ts
+++ Demos/04_formulaires/src/app/components/formulaire/formulaire.ts
@@ -1,16 +1,37 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulaire',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './formulaire.html',
   styleUrl: './formulaire.css',
 })
 export class Formulaire {
   message : string = "test"
+  message2 : string = ""
+
+  user = {
+    name: '',
+    email: '',
+    password: ''
+  }
+
+  isSubmitted = false
 
   updateMessage(e : Event) {
     const input = e.target as HTMLInputElement
     this.message = input.value
+  }
+
+  get passwordHasError(){
+    return this.isSubmitted && this.user.password.length < 6
+  }
+
+  submitUser(){
+    this.isSubmitted = true
+    if (!this.passwordHasError){
+      console.log(this.user)
+    }
   }
 }
